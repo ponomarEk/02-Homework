7d8148887d568fd0f3940056cc5f94ebba42dfc6
Sasha <ponomarek91@gmail.com>
2020-01-31 20:55:59 +0200

    update

--- Homework_2/02-Homework.js
+++ Homework_2/02-Homework.js
@@ -1,20 +1,22 @@
-let numberN = prompt("Input nuber N : ");
-let numberM = prompt("Input nuber M : ");
+let numberN = prompt("Input number N : ");
+let numberM = prompt("Input number M : ");
 let flag = true;
 while(flag){
-if(parseFloat(numberN)-parseInt(numberN)!==0 ||parseFloat(numberM)-parseInt(numberM)!==0){
-    alert("not integer number!!!")
-    numberN = prompt("Input nuber N : ");
-    numberM = prompt("Input nuber M : ");
+if(parseFloat(numberN)-parseInt(numberN)!==0 || parseFloat(numberM)-parseInt(numberM)!==0 || parseInt(numberN)>parseInt(numberM)){
+    alert("not integer number or N > M!!!")
+    numberN = prompt("Input number N : ");
+    numberM = prompt("Input number M : ");
 }else{
     flag = false;
     numberN=parseInt(numberN);
     numberM=parseInt(numberM);
 }
 }
+
 console.log(numberM);
 console.log(numberN);
 let missEven = confirm("Miss?");
+console.log(missEven);
 let sum=0;
     if(missEven===false){
         for(let i = numberN;i<=numberM;i++){
@@ -29,3 +31,9 @@ else{
 }
 console.log(sum);
 
+let homeworkTemplate = `
+number N = ${numberN}<br>
+number M = ${numberM}<br>
+missEven = ${missEven}<br>
+sum = ${sum}<br>`
+document.writeln(homeworkTemplate);
