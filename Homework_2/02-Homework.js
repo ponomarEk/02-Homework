39c25294fcca8d434fb0404404ec956047619064
ponomarEk <60274039+ponomarEk@users.noreply.github.com>
2020-01-31 21:35:21 +0200

    Update 02-Homework.js

--- Homework_2/02-Homework.js
+++ Homework_2/02-Homework.js
@@ -1,11 +1,11 @@
-let numberN = prompt("Input number N : ");
-let numberM = prompt("Input number M : ");
+let numberN = +(prompt("Input number N : "));
+let numberM = +(prompt("Input number M : "));
 let flag = true;
 while(flag){
 if(parseFloat(numberN)-parseInt(numberN)!==0 || parseFloat(numberM)-parseInt(numberM)!==0 || parseInt(numberN)>parseInt(numberM)){
     alert("not integer number or N > M!!!")
-    numberN = prompt("Input number N : ");
-    numberM = prompt("Input number M : ");
+    numberN = +(prompt("Input number N : "));
+    numberM = +(prompt("Input number M : "));
 }else{
     flag = false;
     numberN=parseInt(numberN);
@@ -18,7 +18,7 @@ console.log(numberN);
 let missEven = confirm("Miss?");
 console.log(missEven);
 let sum=0;
-    if(missEven===false){
+    if(missEven){
         for(let i = numberN;i<=numberM;i++){
             sum+=i;
     }
