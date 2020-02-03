01e724d6a2299eea6732538685c0dc58548ae5e9
ponomarEk <60274039+ponomarEk@users.noreply.github.com>
2020-02-04 00:55:40 +0200

    Update 02-Homework.js

--- Homework_2/02-Homework.js
+++ Homework_2/02-Homework.js
@@ -1,13 +1,18 @@
-let numberN = +(prompt("Input number N : "));
-let numberM = +(prompt("Input number M : "));
+let numberN = prompt("Input number N : ");
+let numberM = prompt("Input number M : ");
 let flag = true;
 while(flag){
 
-if(parseFloat(numberN)-parseInt(numberN)!==0 || parseFloat(numberM)-parseInt(numberM)!==0 || parseInt(numberN)>=parseInt(numberM)){
+if(parseFloat(numberN)-parseInt(numberN)!==0 || parseFloat(numberM)-parseInt(numberM)!==0 || parseInt(numberN)>=parseInt(numberM) ||isNaN(numberN)!==false || isNaN(namberM)!==false)){
     alert("not integer number or N >= M!!!");
-    numberN = +(prompt("Input number N : "));
-    numberM = +(prompt("Input number M : "));
-}else flag = false;
+    numberN = prompt("Input number N : ");
+    numberM = prompt("Input number M : ");
+}else {flag = false;
+
+numberN=parseInt(numberN);
+
+numberM=parseInt(numberM);}
+
 }
 
 console.log(numberM);
